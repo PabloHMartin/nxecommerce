b7e1245e176d200fd2305e496bfcf8f9084f1896
Pablo Hernandez Martin <pablohmartindev@gmail.com>
2022-07-28 22:16:59 +0200

    added redux project for products feature and imported into my-ecommerce project

--- apps/my-ecommerce/src/app/features/products/products.module.ts
+++ apps/my-ecommerce/src/app/features/products/products.module.ts
@@ -4,9 +4,10 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { ProductsRoutingModule } from './products.routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { ProductsComponent } from './products.component';
+import { ReduxModule } from '@ecommerce/redux';
 
 @NgModule({
   declarations: [CatalogComponent, ProductsComponent],
-  imports: [CommonModule, ProductsRoutingModule, SharedModule],
+  imports: [CommonModule, ProductsRoutingModule, SharedModule, ReduxModule],
 })
 export class ProductsModule {}
